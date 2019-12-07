603d506cc13ca319021881ce1a728265a047ed28
zacharyarney <zachary.arney@gmail.com>
2019-12-07 13:52:16 -0800

    moves DayOne logic out of Main into DayOne

--- src/aoc/DayOne.java
+++ src/aoc/DayOne.java
@@ -1,6 +1,44 @@
 package aoc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DayOne {
+    private int sum = 0;
+    private int realSum = 0;
+    private File input;
+    private Scanner scanner;
+
+    public DayOne(File input) throws FileNotFoundException {
+        this.input = input;
+        scanner = new Scanner(input);
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public int getRealSum() {
+        return realSum;
+    }
+
+    public void setRealSum(int realSum) {
+        this.realSum = realSum;
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     public int fuelCounterUpper(int mass) {
         return mass / 3 - 2;
     }
@@ -15,7 +53,19 @@ public class DayOne {
         } else {
             return required + fuelRequired(required);
         }
-
     }
 
+    public void countUpFuel(Scanner s) {
+        while (s.hasNextInt()) {
+            int mass = s.nextInt();
+
+            sum += fuelCounterUpper(mass);
+            realSum += fuelRequired(mass);
+        }
+
+        System.out.println("===DAY 1.1===");
+        System.out.println("TOTAL FUEL REQUIREMENT: " + sum);
+        System.out.println("\n===DAY 1.2===");
+        System.out.println("ACTUAL TOTAL FUEL REQUIREMENT: " + realSum);
+    }
 }
--- src/aoc/Main.java
+++ src/aoc/Main.java
@@ -2,29 +2,12 @@ package aoc;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println(System.getProperty("user.dir"));
-        DayOne dayOne = new DayOne();
-        File f = new File("aoc/inputs/day1.txt");
-        System.out.println("FILE EXISTS? " + f.exists());
-        Scanner s = new Scanner(f);
-        int sum = 0;
-        int realSum = 0;
+        DayOne dayOne = new DayOne(new File("aoc/inputs/day1.txt"));
 
-        System.out.println("===DAY 1.1===");
-
-        while (s.hasNextInt()) {
-            int mass = s.nextInt();
-
-            sum += dayOne.fuelCounterUpper(mass);
-            realSum += dayOne.fuelRequired(mass);
-        }
-
-        System.out.println("TOTAL FUEL REQUIREMENT: " + sum);
-        System.out.println("\n===DAY 1.2===");
-        System.out.println("ACTUAL TOTAL FUEL REQUIREMENT: " + realSum);
+        dayOne.countUpFuel(dayOne.getScanner());
     }
 }
