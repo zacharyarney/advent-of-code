20e9e6d232c2d2a622bc4897207062da06bbb1e4
zacharyarney <zachary.arney@gmail.com>
2019-12-07 21:18:19 -0800

    DayTwo challenge one ArrayList solution

--- src/aoc/DayTwo.java
+++ src/aoc/DayTwo.java
@@ -44,8 +44,26 @@ public class DayTwo {
            }
         }
 
+        // ArrayList solution
+        for (int i = 0; i < output.size(); i += 4) {
+            int cur = output.get(i);
+
+            if (cur == 1) {
+                output.set(output.get(i + 3), output.get(output.get(i + 1)) + output.get(output.get(i + 2)));
+            } else if (cur == 2) {
+                output.set(output.get(i + 3), output.get(output.get(i + 1)) * output.get(output.get(i + 2)));
+            } else if (cur == 99) {
+                break;
+            } else {
+                System.out.println("ERROR: " + output.get(i) + " is not a valid opcode");
+                break;
+            }
+        }
+
         System.out.println("\n=== DAY 2.1 ===");
         System.out.println("~ Array Solution ~");
         System.out.println("OPCODE 0: " + intArr[0]);
+        System.out.println("~ ArrayList Solution ~");
+        System.out.println("OPCODE 0: " + output.get(0));
     }
 }
