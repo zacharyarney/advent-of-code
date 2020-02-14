cb7f4710119331a8ef70af52bc03b4eaba59b30c
zacharyarney <zachary.arney@gmail.com>
2020-02-13 22:53:50 -0800

    day 4 complete

--- python-solutions/day_4.py
+++ python-solutions/day_4.py
@@ -2,20 +2,36 @@ def find_valid_passwords(start, end):
     count = 0
 
     for i in range(start, end):
-        contains_same_adjacent = False
+        contains_adjacent_pair = False
         increasing = True
         i_string = str(i)
         i_string_prev = None
+        current_run = None
+        pair = False
 
         for digit in i_string:
-            if i_string_prev is not None and int(digit) < i_string_prev:
-                increasing = False
-            if i_string_prev is not None and int(digit) == i_string_prev:
-                contains_same_adjacent = True
+            int_digit = int(digit)
+            if i_string_prev is not None and int_digit != i_string_prev:
+                if current_run is not None and pair is True:
+                    contains_adjacent_pair = True
+                    current_run = None
+                if int_digit < i_string_prev:
+                    increasing = False
 
-            i_string_prev = int(digit)
+            if i_string_prev is not None and int_digit == i_string_prev:
+                if int_digit == current_run:
+                    pair = False
+                else:
+                    pair = True
 
-        if increasing and contains_same_adjacent:
+                current_run = int_digit
+
+            i_string_prev = int_digit
+
+        if pair:
+            contains_adjacent_pair = True
+
+        if increasing and contains_adjacent_pair:
             count += 1
 
     return count
--- python-solutions/main.py
+++ python-solutions/main.py
@@ -2,7 +2,7 @@ from day_4 import find_valid_passwords
 
 
 def main():
-    print('===Day 4.1===')
+    print('===Day 4===')
     print(find_valid_passwords(372304, 847060))
 
 
