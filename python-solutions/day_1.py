8ad6b7f32373e8d8aaed576a541bd0a40f0f1e7f
zacharyarney <zachary.arney@gmail.com>
2020-02-14 20:19:10 -0800

    python day 1.2 complete

--- python-solutions/day_1.py
+++ python-solutions/day_1.py
@@ -3,8 +3,20 @@ from math import floor
 
 def fuel_counter_upper(input):
     modules = open(input).read().splitlines()
+    # Part 1
     # this one-liner is probably not great practice
     # but I know how to write a loop and this is for learning.
-    total_fuel = sum(floor(int(m) / 3) - 2 for m in modules if int(m) > 0)
+    total_fuel = sum(required_fuel(m) for m in modules if int(m) > 0)
 
     return total_fuel
+
+
+def required_fuel(mass):
+    req_fuel = floor(int(mass) / 3) - 2
+
+    if req_fuel > 2:
+        return req_fuel + required_fuel(req_fuel)
+    elif req_fuel < 0:
+        return 0
+    else:
+        return req_fuel
