dc58f82e6116a6c7298abf056c7cda7d0e5bb1e9
zacharyarney <zachary.arney@gmail.com>
2020-02-21 22:58:57 -0800

    day 2.2 complete

--- python-solutions/day_2.py
+++ python-solutions/day_2.py
@@ -1,17 +1,25 @@
-def intcode_reader(input):
-    input[1] = 12
-    input[2] = 2
+def intcode_reader(input, noun=12, verb=2):
+    arr = input.copy()
+    arr[1] = noun
+    arr[2] = verb
 
-    for i in range(0, len(input), 4):
-        opcode = input[i]
-        arg_1 = input[input[i+1]]
-        arg_2 = input[input[i+2]]
+    for i in range(0, len(arr), 4):
+        opcode = arr[i]
+        arg_1 = arr[arr[i+1]]
+        arg_2 = arr[arr[i+2]]
 
-        if input[i] == 1:
-            input[input[i+3]] = arg_1 + arg_2
-        elif input[i] == 2:
-            input[input[i+3]] = arg_1 * arg_2
-        elif input[i] == 99:
-            return input[0]
+        if arr[i] == 1:
+            arr[arr[i+3]] = arg_1 + arg_2
+        elif arr[i] == 2:
+            arr[arr[i+3]] = arg_1 * arg_2
+        elif arr[i] == 99:
+            return arr[0]
         else:
             return 'Something is wrong here...'
+
+
+def find_input(input_arr, output):
+    for i in range(len(input_arr)):
+        for j in range(len(input_arr)):
+            if intcode_reader(input_arr, i, j) == output:
+                return f'INPUT CODE: {100 * i + j}'
--- python-solutions/main.py
+++ python-solutions/main.py
@@ -2,13 +2,13 @@ import os
 import sys
 from inputs import day2
 from day_1 import fuel_counter_upper
-from day_2 import intcode_reader
+from day_2 import intcode_reader, find_input
 from day_4 import find_valid_passwords
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-day_1_input = '%s/inputs/day1.txt' % base_dir
+day_1_input = f'{base_dir}/inputs/day1.txt'
 
 
 def main():
@@ -16,6 +16,7 @@ def main():
     print(fuel_counter_upper(day_1_input))
     print('===Day 2===')
     print(intcode_reader(day2.input))
+    print(find_input(day2.input, 19690720))
     print('===Day 4===')
     print(find_valid_passwords(372304, 847060))
 
