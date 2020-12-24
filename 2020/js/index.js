fc79feda2794d798015f0eb2c170fff0a0baa8e3
zacharyarney <zachary.arney@gmail.com>
2020-12-24 16:41:38 -0600

    adds day03 to index.js

--- 2020/js/index.js
+++ 2020/js/index.js
@@ -1,11 +1,29 @@
+const path = require('path');
+const { twoDArray } = require('./util/read');
 const { dayTwoPtOne, dayTwoPtTwo } = require('./day02/day02');
 const { input1 } = require('./day02/input1');
+const { dayThreePtOne, dayThreePtTwo } = require('./day03/day03');
+const dayThreeInputOnePath = path.resolve(
+  __dirname,
+  'day03/dayThreeInputOne.txt'
+);
+const dayThreePtTwoPathArr = [
+  [1, 1],
+  [1, 3],
+  [1, 5],
+  [1, 7],
+  [2, 1],
+];
 
-function main() {
+async function main() {
   console.log('ADVENT OF CODE 2020');
-  console.log('Day One');
-  console.log(`Part 1: ${dayTwoPtOne(input1)}`);
-  console.log(`Part 2: ${dayTwoPtTwo(input1)}`);
+  console.log('Day Two');
+  console.log('\tPart 1: ', dayTwoPtOne(input1));
+  console.log('\tPart 2: ', dayTwoPtTwo(input1));
+  console.log('Day Three');
+  const dayThreeInputOne = await twoDArray(dayThreeInputOnePath);
+  console.log('\tPart 1: ', dayThreePtOne(dayThreeInputOne, 1, 3));
+  console.log('\tPart 2: ', dayThreePtTwo(dayThreeInputOne, dayThreePtTwoPathArr));
 }
 
 main();
